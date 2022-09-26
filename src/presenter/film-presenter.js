4d2ac18e437b803dc3e6cebf60812355fa03fd71
Dmitry <sheklein.dima@gmail.com>
2022-09-26 18:49:13 +0300

    Добавит возможность переиспользования

--- src/presenter/film-presenter.js
+++ src/presenter/film-presenter.js
@@ -1,4 +1,4 @@
-import { render, remove } from '../framework/render';
+import { render, remove, replace } from '../framework/render';
 import FilmsCard from '../view/film-card';
 import FilmPopupPresenter from './film-popup-presenter';
 
@@ -15,10 +15,26 @@ export default class FilmPresenter {
 
   init = (film) => {
     this.#film = film;
+    const prevFilmComponent = this.#filmComponent;
     this.#filmComponent = new FilmsCard(this.#film);
     this.#filmComponent.setClickHandler(this.#onFilmCardClick);
-    render(this.#filmComponent, this.#container);
+
+    if (prevFilmComponent === null) {
+      render(this.#filmComponent, this.#container);
+
+      return;
+    }
+
+    if (this.#container.contains(prevFilmComponent.element)) {
+      replace(this.#filmComponent, prevFilmComponent);
+    }
+
+    remove(prevFilmComponent);
   };
 
   #onFilmCardClick = (film) => new FilmPopupPresenter(this.#commentsModel).init(film);
+
+  destroy = () => {
+    remove(this.#filmComponent);
+  };
 }
