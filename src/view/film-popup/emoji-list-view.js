83361474ab00cd88d8d8ae7201e9746e9db6cc7f
Dmitry <sheklein.dima@gmail.com>
2022-09-30 10:12:31 +0300

    Eslint fix

--- src/view/film-popup/emoji-list-view.js
+++ src/view/film-popup/emoji-list-view.js
@@ -5,7 +5,7 @@ const createEmojiList = (checkedEmotion) =>
     .map(
       (emotion) => `
     <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emotion}" value="${emotion}" ${
-      emotion === checkedEmotion ? 'checked' : ''
+  emotion === checkedEmotion ? 'checked' : ''
 }>
     <label class="film-details__emoji-label" for="emoji-${emotion}">
       <img src="./images/emoji/${emotion}.png" width="30" height="30" alt="emoji">
