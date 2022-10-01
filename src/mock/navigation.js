4f1169141c1dcfe1e0e31a5ebbe8362064cfbbef
Dmitry <sheklein.dima@gmail.com>
2022-10-01 20:41:12 +0300

    Удалит моковую навигацию

--- src/mock/navigation.js
+++
@@ -1,9 +0,0 @@
-import { NavigationFilter } from '../util';
-import { NavigationType } from '../main-const';
-
-export const generateNavigation = (films) =>
-  Object.entries(NavigationFilter).map(([filterId, filterFunction]) => ({
-    filterId,
-    filterName: NavigationType[filterId.toLocaleUpperCase()]?.name,
-    filterCount: filterFunction(films).length,
-  }));
