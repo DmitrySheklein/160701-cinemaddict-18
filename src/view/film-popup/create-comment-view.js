fc6355b96b56fe6432d38ac79c5f2aa950ea0a12
Dmitry <sheklein.dima@gmail.com>
2022-10-03 10:52:34 +0300

    Реализует безопасный ввод

--- src/view/film-popup/create-comment-view.js
+++ src/view/film-popup/create-comment-view.js
@@ -1,4 +1,5 @@
 import { HumanizeDate } from '../../util';
+import he from 'he';
 
 const createComment = (commentItem) => {
   const { id, comment, emotion, author = '', date } = commentItem;
@@ -9,7 +10,7 @@ const createComment = (commentItem) => {
       <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">
     </span>
     <div>
-      <p class="film-details__comment-text">${comment}</p>
+      <p class="film-details__comment-text">${he.encode(comment)}</p>
       <p class="film-details__comment-info">
         <span class="film-details__comment-author">${author}</span>
         <span class="film-details__comment-day">${HumanizeDate.FromNow(date)}</span>
