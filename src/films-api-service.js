61528e88523989b184148e1fbb47ad1761162975
Dmitry <sheklein.dima@gmail.com>
2022-10-04 20:26:01 +0300

    Создаст класс для работы с API

---
+++ src/films-api-service.js
@@ -0,0 +1,24 @@
+import ApiService from './framework/api-service';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class FilmsApiService extends ApiService {
+  get films() {
+    return this._load({ url: 'movies' }).then(ApiService.parseResponse);
+  }
+
+  updateFilm = async (film) => {
+    const response = await this._load({
+      url: `movies/${film.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(film),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    });
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  };
+}
