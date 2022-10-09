57f008d613f663aacd3f09f21f97e53e14a39ab3
Dmitry <sheklein.dima@gmail.com>
2022-10-09 18:59:59 +0300

    Модифицирует FilmsListSection

--- src/view/films-list/films-list-section-view.js
+++ src/view/films-list/films-list-section-view.js
@@ -1,9 +1,17 @@
 import AbstractView from '../../framework/view/abstract-view';
 
-const createFilmsListSectionTemplate = () => '<section class="films-list"></section>';
+const createFilmsListSectionTemplate = (isExtra) =>
+  `<section class="films-list ${isExtra ? 'films-list--extra' : ''}"></section>`;
 
 export default class FilmsListSection extends AbstractView {
+  #isExtra = null;
+
+  constructor(isExtra = false) {
+    super();
+    this.#isExtra = isExtra;
+  }
+
   get template() {
-    return createFilmsListSectionTemplate();
+    return createFilmsListSectionTemplate(this.#isExtra);
   }
 }
